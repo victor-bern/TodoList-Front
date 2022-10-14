6fbf4a73ab6d8d2b612b2851e7639252d1d8ab14
Victor Bernardes <dartinjs@gmail.com>
2022-10-14 11:24:27 -0300

    adding icon to done todo

--- src/components/TodoItem/Index.tsx
+++ src/components/TodoItem/Index.tsx
@@ -1,23 +1,31 @@
 import { changeStatustodo } from '../../services/todoService';
 import { ITodo } from '../../types/ITodo';
+import { FiCheck } from 'react-icons/fi';
 import { CheckBox, Container, TodoTitle } from './styles';
+import { useEffect, useState } from 'react';
 
 type TodoItemProps = {
   todo: ITodo;
 };
 const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const [isDone, setIsDone] = useState(todo.isDone);
   const onCheckHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
     await changeStatustodo(todo.id!);
+    setIsDone(true);
   };
-
+  useEffect(() => {}, [todo.isDone]);
   return (
     <Container>
       <TodoTitle>{todo.title}</TodoTitle>
-      <CheckBox
-        type='checkbox'
-        onChange={onCheckHandler}
-        defaultChecked={todo.isDone}
-      />
+      {isDone ? (
+        <FiCheck />
+      ) : (
+        <CheckBox
+          type='checkbox'
+          onChange={onCheckHandler}
+          defaultChecked={todo.isDone}
+        />
+      )}
     </Container>
   );
 };
